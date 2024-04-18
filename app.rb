12d1085371d55a63c15ff00e5b82044e30997fdd
cmackie1 <165298882+cmackie1@users.noreply.github.com>
2024-04-18 16:28:39 +0000

    1

--- app.rb
+++ app.rb
@@ -5,13 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
-
+  @second_num = params.fetch("second_num").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
@@ -32,6 +32,8 @@ get("/multiply") do
 end
 
 get("/wizard multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
   erb(:mult_result)
 end
 
@@ -40,5 +42,7 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
   erb(:div_result)
 end
